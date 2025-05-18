3d019c7952cc35a3e687431fb8b65d3554780c7b
Paul O'Neill <paul_oneill@apple.com>
2025-05-17 19:37:57 -0600

    Create Triangle.swift

---
+++ ShapeViewerApp/Models/Triangle.swift
@@ -0,0 +1,22 @@
+//
+//  Triangle.swift
+//  ShapeViewerApp
+//
+//  Created by Paul O'Neill on 5/17/25.
+//
+
+import SwiftUI
+
+struct Triangle: Shape {
+	func path(in rect: CGRect) -> Path {
+		var path = Path()
+		
+		path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+		path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+		path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+		path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+		path.closeSubpath()
+		
+		return path
+	}
+}
